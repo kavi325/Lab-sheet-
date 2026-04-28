c39d2fcde3e957a1d2b6782b0f338509184caa1c
kavi325 <knimsara101@gmail.com>
2026-04-28 14:58:43 +0530

    Create Question 6.py

---
+++ Question 6.py
@@ -0,0 +1,11 @@
+i=1
+while i<=3:
+    x=str(input("Enter the Password: "))
+    if x=="admin":
+        print("Loggin Sucsessful")
+    else:
+         i=i+1
+if i<=3:
+    print("Account Locked")
+
+        
