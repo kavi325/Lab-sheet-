be9e50bc9d9a65d6ce67b9b5526a589555f27c22
kavi325 <knimsara101@gmail.com>
2026-04-28 14:23:31 +0530

    Create Lab sheet.py

---
+++ Lab sheet.py
@@ -0,0 +1,15 @@
+#Question 01
+i=1
+y=0
+while i<=5:
+    x=int(input("Enter the marks"))
+    y=y+x
+    i=i+1
+Avg =y/5 
+if Avg>=50:
+    print("Pass")
+else:
+    print("Fail")
+
+#Question 02
+
